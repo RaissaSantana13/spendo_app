e8d08cd1451d99ef507b90917559aed0e5b826c7
Raissa Santana <raissasanta8@gmail.com>
2026-04-14 10:24:32 -0300

    add_chart

--- lib/models/widgets/expenses.dart
+++ lib/models/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spendo_app/models/widgets/chart/chart.dart';
 import 'package:spendo_app/models/widgets/expenses_list/expenses_list.dart';
 import 'package:spendo_app/models/expense.dart';
 import 'package:spendo_app/models/widgets/new_expense.dart';
@@ -64,20 +65,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          Container(
-            width: double.infinity,
-            margin: const EdgeInsets.all(16),
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              color: Theme.of(context).colorScheme.primary.withOpacity(0.1),
-            ),
-            child: Text(
-              'The chart',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: Theme.of(context).colorScheme.primary),
-            ),
-          ),
+          Chart(expenses: _registeredExpenses),
           Expanded(child: mainContent),
         ],
       ),
